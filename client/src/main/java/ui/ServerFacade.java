662c739485a334f2608dd78752387a5363fa6cdd
Dylan-Harps <dylaniharps@gmail.com>
2025-03-20 20:35:08 -0600

    Added ServerFacade class without functionality

---
+++ client/src/main/java/ui/ServerFacade.java
@@ -0,0 +1,50 @@
+package ui;
+
+import chess.ChessGame;
+
+public class ServerFacade {
+
+    public void preLoginHelp() {
+        //TODO
+    }
+
+    public void quit() {
+        //TODO
+    }
+
+    public void login() {
+        //TODO
+    }
+
+    public void register() {
+        //TODO
+    }
+
+    public void postLoginHelp() {
+        //TODO
+    }
+
+    public void logout() {
+        //TODO
+    }
+
+    public void createGame() {
+        //TODO
+    }
+
+    public void listGames() {
+        //TODO
+    }
+
+    public void playGame() {
+        //TODO
+    }
+
+    public void observeGame() {
+        //TODO
+    }
+
+    private void displayGame(ChessGame.TeamColor perspective) {
+        //TODO
+    }
+}
