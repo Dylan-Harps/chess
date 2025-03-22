8e2258a6cb05dc1f99157de47b65a5a757d4edf5
Dylan-Harps <dylaniharps@gmail.com>
2025-03-21 18:27:25 -0600

    Added methods to Client Main without functionality and made progress on adding functionality to ServerFacade

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,58 @@
 import chess.*;
+import ui.ServerFacade;
 
 public class Main {
+    ServerFacade serverFacade = new ServerFacade();
+
+    public Main() throws Exception {
+    }
+
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+    }
+
+    public void preLoginHelp() {
+        //TODO
+    }
+
+    public void quit() {
+        //TODO
+    }
+
+    public void login() {
+        //TODO
+    }
+
+    public void register() {
+        //TODO
+    }
+
+    public void postLoginHelp() {
+        //TODO
+    }
+
+    public void logout() {
+        //TODO
+    }
+
+    public void createGame() {
+        //TODO
+    }
+
+    public void listGames() {
+        //TODO
+    }
+
+    public void playGame() {
+        //TODO
+    }
+
+    public void observeGame() {
+        //TODO
+    }
+
+    private void displayGame(ChessGame.TeamColor perspective) {
+        //TODO
     }
 }
--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -1,50 +1,60 @@
 package ui;
 
 import chess.ChessGame;
+import com.google.gson.Gson;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.util.Map;
 
 public class ServerFacade {
+    HttpURLConnection http;
 
-    public void preLoginHelp() {
-        //TODO
+    public ServerFacade() throws Exception {
+        // Specify the desired endpoint
+        URI uri = new URI("http://localhost:3306/chess");
+        http = (HttpURLConnection) uri.toURL().openConnection();
     }
 
-    public void quit() {
-        //TODO
+    public void example() throws Exception {
+        // Make the request
+        http.setRequestMethod("GET");
+        http.connect();
+
+        // Output the response body
+        try (InputStream respBody = http.getInputStream()) {
+            InputStreamReader inputStreamReader = new InputStreamReader(respBody);
+            System.out.println(new Gson().fromJson(inputStreamReader, Map.class));
+        }
+
+        //example of outputting to error stream or output stream
+        // Handle bad HTTP status
+        var status = http.getResponseCode();
+        if ( status >= 200 && status < 300) {
+            try (InputStream in = http.getInputStream()) {
+                System.out.println(new Gson().fromJson(new InputStreamReader(in), Map.class));
+            }
+        } else {
+            try (InputStream in = http.getErrorStream()) {
+                System.out.println(new Gson().fromJson(new InputStreamReader(in), Map.class));
+            }
+        }
     }
 
-    public void login() {
-        //TODO
-    }
+    private void makeRequest() throws Exception {
+        // Specify that we are going to write out data
+        http.setDoOutput(true);
 
-    public void register() {
-        //TODO
-    }
+        // Write out a header
+        http.addRequestProperty("HeaderName", "HeaderContents");
 
-    public void postLoginHelp() {
-        //TODO
-    }
-
-    public void logout() {
-        //TODO
-    }
-
-    public void createGame() {
-        //TODO
-    }
-
-    public void listGames() {
-        //TODO
-    }
-
-    public void playGame() {
-        //TODO
-    }
-
-    public void observeGame() {
-        //TODO
-    }
-
-    private void displayGame(ChessGame.TeamColor perspective) {
-        //TODO
+        // Write out the body
+        var body = Map.of("name", "joe", "type", "cat");
+        try (var outputStream = http.getOutputStream()) {
+            var jsonBody = new Gson().toJson(body);
+            outputStream.write(jsonBody.getBytes());
+        }
     }
 }
