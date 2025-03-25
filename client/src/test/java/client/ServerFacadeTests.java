786ef1fc977167d20d9e88515b59966a8a64b764
Dylan-Harps <dylaniharps@gmail.com>
2025-03-25 15:27:32 -0600

    Added and passed register test in ServerFacadeTests.

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -2,17 +2,29 @@ package client;
 
 import org.junit.jupiter.api.*;
 import server.Server;
+import service.*;
+import ui.ServerFacade;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
+    private static String username = "username";
+    private static String password = "password";
+    private static String email = "email";
 
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(0);
+        var port = server.run(8080);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:8080");
+    }
+
+    @BeforeEach
+    void clearDatabase() {
+        facade.clear(new ClearRequest());
     }
 
     @AfterAll
@@ -24,6 +36,13 @@ public class ServerFacadeTests {
     @Test
     public void sampleTest() {
         Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void registerTest() {
+        RegisterResult result = facade.registerUser(new RegisterRequest(username, password, email));
+        Assertions.assertFalse(result.username().isEmpty());
+        Assertions.assertFalse(result.authToken().isEmpty());
     }
 
 }
