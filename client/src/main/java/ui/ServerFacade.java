26d4bfb41a0c789f1b74a273f73c7c96b867be64
Dylan-Harps <dylaniharps@gmail.com>
2025-03-25 13:49:13 -0600

    Added methods to ServerFacade based on PetShop. Not fully functional yet.

--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -1,60 +1,115 @@
 package ui;
 
-import chess.ChessGame;
 import com.google.gson.Gson;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
-import java.util.Map;
+import java.net.URL;
+import handler.ResponseException;
+import service.*;
+
 
 public class ServerFacade {
-    HttpURLConnection http;
+    private final String serverUrl;
 
-    public ServerFacade() throws Exception {
-        // Specify the desired endpoint
-        URI uri = new URI("http://localhost:3306/chess");
-        http = (HttpURLConnection) uri.toURL().openConnection();
+    public ServerFacade(String url) {
+        serverUrl = url;
     }
 
-    public void example() throws Exception {
-        // Make the request
-        http.setRequestMethod("GET");
-        http.connect();
+    public RegisterResult registerUser(RegisterRequest request) throws ResponseException {
+        var path = "/user";
+        return this.makeRequest("POST", path, request, RegisterResult.class);
+    }
 
-        // Output the response body
-        try (InputStream respBody = http.getInputStream()) {
-            InputStreamReader inputStreamReader = new InputStreamReader(respBody);
-            System.out.println(new Gson().fromJson(inputStreamReader, Map.class));
+    public LoginResult loginUser(LoginRequest request) throws ResponseException {
+        var path = "/session";
+        return this.makeRequest("POST", path, request, LoginResult.class);
+    }
+
+    public LogoutResult logoutUser(LogoutRequest request) throws ResponseException {
+        var path = "/session";
+        return this.makeRequest("DELETE", path, request, LogoutResult.class);
+    }
+
+    public ListGamesResult listGames(ListGamesRequest request) throws ResponseException {
+        var path = "/game";
+        return this.makeRequest("GET", path, request, ListGamesResult.class);
+    }
+
+    public CreateGameResult createGame(CreateGameRequest request) throws ResponseException {
+        var path = "/game";
+        return this.makeRequest("POST", path, request, CreateGameResult.class);
+    }
+
+    public JoinGameResult joinGame(CreateGameRequest request) throws ResponseException {
+        var path = "/game";
+        return this.makeRequest("PUT", path, request, JoinGameResult.class);
+    }
+
+    public ClearResult clear(ClearRequest request) throws ResponseException {
+        var path = "/db";
+        return this.makeRequest("DELETE", path, request, ClearResult.class);
+    }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (ResponseException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
         }
+    }
 
-        //example of outputting to error stream or output stream
-        // Handle bad HTTP status
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json"); //TODO
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
         var status = http.getResponseCode();
-        if ( status >= 200 && status < 300) {
-            try (InputStream in = http.getInputStream()) {
-                System.out.println(new Gson().fromJson(new InputStreamReader(in), Map.class));
+        if (!isSuccessful(status)) {
+            try (InputStream respErr = http.getErrorStream()) {
+                if (respErr != null) {
+                    throw ResponseException.fromJson(respErr);
+                }
             }
-        } else {
-            try (InputStream in = http.getErrorStream()) {
-                System.out.println(new Gson().fromJson(new InputStreamReader(in), Map.class));
-            }
+
+            throw new ResponseException(status, "other failure: " + status);
         }
     }
 
-    private void makeRequest() throws Exception {
-        // Specify that we are going to write out data
-        http.setDoOutput(true);
-
-        // Write out a header
-        http.addRequestProperty("HeaderName", "HeaderContents");
-
-        // Write out the body
-        var body = Map.of("name", "joe", "type", "cat");
-        try (var outputStream = http.getOutputStream()) {
-            var jsonBody = new Gson().toJson(body);
-            outputStream.write(jsonBody.getBytes());
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
         }
+        return response;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
     }
 }
